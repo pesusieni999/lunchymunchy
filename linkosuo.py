839993e0e2ef9423d29c735f7b5a17f94f1434bc
pesusieni999 <pesusieni999@gmail.com>
2017-02-17 10:35:06 +0200

    Initial commit

---
+++ linkosuo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def get_lunch_menu():
+    menu = ""
+    return menu
+
+
+def send_data(data):
+    pass
+
+
+def main():
+    menu = get_lunch_menu()
+    send_data(menu)
+    print("Lunch menu is:\n", menu)
+
+if __name__ == "__main__":
+    main()
+
